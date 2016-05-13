bb406ebb89f9d401eddf68188b6c15f58a2688a3
Jens Klingsporn <klingsporn@badmonkee.de>
2016-05-13 16:07:20 +0200

    npc is going to kill you

--- unity_project/multiquest/Assets/Scripts/MVC/View/Characters/AI/Behaviours/AttackAction.cs
+++ unity_project/multiquest/Assets/Scripts/MVC/View/Characters/AI/Behaviours/AttackAction.cs
@@ -1,5 +1,7 @@
 ﻿using TreeSharp;
-
+using MVC.Model.Character;
+using MVC.View.Characters.MonoBehaviours;
+using UnityEngine;
 namespace MVC.View.Characters.AI.Behaviours
 {
 	public class AttackAction : AbstractAction
@@ -7,7 +9,17 @@ namespace MVC.View.Characters.AI.Behaviours
 		
 
 		protected override RunStatus Run(object context){
-			UnityEngine.Debug.Log ("attack");
+			NPCRepresentation npcRepresentation = this.Unwrap (context);
+			CharacterProxy proxy = npcRepresentation.GetFacade ().GetProxy<CharacterProxy> ();
+			foreach (var player in proxy.Players) {
+				foreach (var blockedItem in npcRepresentation.blockedDirections) {
+					if (blockedItem.Collider.GetComponent<CharacterRepresentation> () == player) {
+						npcRepresentation.Attack (blockedItem.Direction);
+						return RunStatus.Success;
+					}
+				}	
+			}
+
 			return RunStatus.Failure;
 		}
 	}
